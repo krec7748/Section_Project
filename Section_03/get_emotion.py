bb2879480c31585cf36042a059ced47a27d18df5
krec7748 <86844420+krec7748@users.noreply.github.com>
2021-10-09 01:53:33 +0900

    Add files via upload

---
+++ Section_03/get_emotion.py
@@ -0,0 +1,33 @@
+import json
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, EmotionOptions
+
+def get_emotion(data, sadness_list = [], joy_list = [], fear_list = [], disgust_list = [], anger_list = []):
+    #IBM_Natural_Language_Understanding API >>> Extracting emotions from text
+    #data = list or Series ex) data = ["I love an apple.", "I hate the smell of cigarettes."]
+    
+    API_KEY = "6EjLFuh21saupPE2yxwCxXkTL3Kxh_TlzCgNMkb8lQZb"
+    URL = "https://api.kr-seo.natural-language-understanding.watson.cloud.ibm.com/instances/bcb16dc6-5939-4300-b089-61944e7892c5"
+
+    authenticator = IAMAuthenticator(API_KEY)
+    natural_language_understanding = NaturalLanguageUnderstandingV1(version='2021-08-01',authenticator=authenticator)
+    natural_language_understanding.set_service_url(URL)
+    try:
+        for sentence in data[len(anger_list):]:
+            response = natural_language_understanding.analyze(text = sentence, features=Features(emotion=EmotionOptions()), language='en').get_result()
+            json_emotion = json.loads(json.dumps(response))["emotion"]["document"]["emotion"]
+    
+            sadness_list.append(json_emotion["sadness"])
+            joy_list.append(json_emotion["joy"])
+            fear_list.append(json_emotion["fear"])
+            disgust_list.append(json_emotion["disgust"])
+            anger_list.append(json_emotion["anger"])
+    except:
+        print(len(anger_list))
+        if len(anger_list) == len(data):
+            pass
+        else:
+            print("API error") #get_emotion() #유료버전일 때 사용.
+    finally:
+        return sadness_list, joy_list, fear_list, disgust_list, anger_list
